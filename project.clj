b49894c0a27c18db36eb744c6c4da782134e3108
Tim Visher <tim.visher@gmail.com>
2013-01-09 09:09:44 -0500

    Spike start. EOM

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject org.clojars.timvisher/lein-checkout "0.1.0-SNAPSHOT"
   :description "Manage your checkouts directory with ease"
-  :url ""
+  :url "https://github.com/timvisher/lein-checkout"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[fs "1.3.3"]]
   :eval-in-leiningen true)
--- src/leiningen/checkout.clj
+++ src/leiningen/checkout.clj
@@ -1,5 +1,6 @@
 (ns leiningen.checkout
-  (:require [clojure.pprint]))
+  (:require [clojure.pprint]
+            [fs.core :as fs]))
 
 (defn pprint-str [o]
   (let [w (java.io.StringWriter.)]
@@ -25,6 +26,63 @@
   "Enable checkouts."
   []
   (println "Amazing enabling action!"))
+
+(comment
+  (def *charnock* (atom []))
+  )
+
+(defn lein-project? [dir]
+  (swap! *charnock* conj (fs/with-cwd  (fs/file dir "project.clj")))
+  (fs/exists? (fs/file dir "project.clj")))
+
+(defn checkout-candidates-in-dir [dir]
+  (if (:path dir))
+  (map fs/parent (filter fs/exists? (map (comp #(fs/file % "project.clj") (partial fs/file dir)) (fs/list-dir dir)))))
+
+(comment
+  (checkout-candidates-in-dir {:path "/Users/tvisher/projects" :search-depth 2})
+  (fs/find-files "/Users/tvisher/projects" #"project.clj")
+
+  (defn list-dir-absolute [dir]
+    (map (partial fs/file dir) (flatten (map (comp fs/list-dir) [dir]))))
+
+  (defn list-dir-absolute [dir]
+    (map (partial fs/file dir) (fs/list-dir dir)))
+
+  (loop [times (seq (range 0 (- 3 1)))
+         candidates (list-dir-absolute "/Users/tvisher/projects")]
+    (if (not times)
+      candidates
+      (recur (next times) (into candidates (flatten (map list-dir-absolute candidates))))))
+
+  (flatten (map list-dir-absolute (flatten (map list-dir-absolute (list-dir-absolute "/Users/tvisher/projects")))))
+
+;;; for search depth
+;;; list-dir
+;;; filter for dirs
+;;; list-dirs in them
+;;; filter for dirs
+  )
+
+
+(defn checkout-candidates
+  "List of checkout candidates in the parent directory and other directories listed in the :checkout map in `:user` and `project.clj`"
+  ([]
+     (checkout-candidates (fs/parent fs/*cwd*)))
+  ([& checkout-roots]
+     (let [checkout-roots (into #{} checkout-roots)
+           checkout-roots (if-not (checkout-roots (fs/parent fs/*cwd*))
+                            (conj checkout-roots (fs/parent fs/*cwd*)))]
+       (def *charnock* checkout-roots)
+       (reduce into [] (map checkout-candidates-in-dir checkout-roots)))))
+
+(comment
+  (checkout-candidates {:path "/Users/tvisher/projects" :search-depth 2})
+  (map checkout-candidates-in-dir *charnock*)
+
+  keep
+  ((into #{} [(fs/parent fs/*cwd*)]) (fs/parent fs/*cwd*))
+  )
 
 (defn
   ^{:subtasks [#'leiningen.checkout/ln
