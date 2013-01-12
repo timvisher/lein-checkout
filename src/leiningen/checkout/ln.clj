1e42bda714e20f46833a9f83635ab20c2feddf3c
Tim Visher <tim.visher@gmail.com>
2013-01-12 08:37:45 -0500

    Further along. Shelling out to `ln` because Java < 7 provides no interface for symlinks. :( EOM

--- src/leiningen/checkout/ln.clj
+++ src/leiningen/checkout/ln.clj
@@ -1,5 +1,7 @@
 (ns leiningen.checkout.ln
-  (:require [fs.core :as fs]))
+  (:require [fs.core            :as fs]
+            [clojure.java.shell :as shell])
+  (:use [leiningen.checkout.enable :only [enable]]))
 
 (defn lein-project? [dir]
   (fs/exists? (fs/file dir "project.clj")))
@@ -27,20 +29,45 @@
                             checkout-roots)]
        (filter lein-project? (reduce into [] (map checkout-candidates-in-dir checkout-roots))))))
 
+(defn link-to-checkouts [source & sources]
+  (let [directory-exists? #(and (fs/exists? %) (fs/directory? %))]
+    (when (not (directory-exists? "checkouts"))
+      (fs/mkdir "checkouts"))
+    (let [sources      (conj sources source)
+          sources      (filter (complement directory-exists?) sources)
+          source-paths (map fs/absolute-path sources)
+          command      (flatten ["ln" "-s" source-paths "checkouts/"])]
+      (apply shell/sh command))))
+
+(comment
+  (let [parent   (fs/expand-home "~/tmp/projects")
+        sources  (map (comp fs/absolute-path (partial fs/file parent)) (fs/list-dir parent))
+        target   (fs/expand-home "~")]
+    (apply link-to-checkouts target sources))
+  (fs/directory? (fs/expand-home "~/a"))
+
+  (conj nil "a")
+  (shell/sh "ls" "-aul" (fs/absolute-path (fs/expand-home "~")))
+  (shell/sh "pwd")
+  (require '[clojure.reflect :as r])
+  (clojure.pprint/pprint (:members (r/reflect String)))
+  )
+
 (defn ln
   "[pattern]: Link project(s) into checkouts. If PATTERN is specified, link all projects matching `.*PATTERN.*`."
   [& [pattern]]
-  
-  (println "Candidates:")
-  (let [candidates-for-checkout            (checkout-candidates {:path "/Users/tvisher/projects" :search-depth 2})
-        candidates-for-checkout-base-names (map fs/base-name candidates-for-checkout)
-        candidate-pattern                  (if pattern (re-pattern (str ".*" pattern ".*")) #".*")
-        candidate-matcher                  (partial re-matches candidate-pattern)
-        print-base-name                    (comp println fs/base-name)
-        candidates                         (sort (filter candidate-matcher candidates-for-checkout-base-names))]
-   (dorun
-    (map print-base-name candidates))
-   candidates))
+  (let [candidates-for-checkout                   (checkout-candidates {:path "/Users/tvisher/projects" :search-depth 2})
+        candidate-pattern                         (if pattern (re-pattern (str ".*" pattern ".*")) ".*")
+        candidate-matcher                         (comp (partial re-matches candidate-pattern) fs/base-name)
+        candidates-for-checkout                   (filter candidate-matcher candidates-for-checkout)
+        sorted-candidates-for-checkout            (sort-by fs/base-name candidates-for-checkout)
+        sorted-candidates-for-checkout-base-names (map fs/base-name sorted-candidates-for-checkout)]
+    (println "Linking:")
+    (dorun
+     (map println sorted-candidates-for-checkout-base-names))
+    (println "into checkouts…")
+    (apply link-to-checkouts sorted-candidates-for-checkout)
+    candidates-for-checkout))
 
 (comment
   (ln "clie")
