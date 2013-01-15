7559a3c889c452f15f092cf6c99e59fa8ed7a5dd
Tim Visher <tim.visher@gmail.com>
2013-01-14 20:12:24 -0500

    Implementing rm. EOM

--- src/leiningen/checkout/rm.clj
+++ src/leiningen/checkout/rm.clj
@@ -1,6 +1,30 @@
-(ns leiningen.checkout.rm)
+(ns leiningen.checkout.rm
+  (:require [fs.core :as fs]))
+
+(defn report-no-matches [non-matched-pattern candidates]
+  (println (str "No matching projects found for: \""
+                non-matched-pattern
+                "\" in checkouts!"))
+  (println "Candidates:")
+  (dorun
+   (map println candidates))
+  candidates)
+
+(defn rm-checkouts [pattern checkouts-to-rm]
+  (println (str "`rm`ing the following projects matching: \"" pattern "\":"))
+  (map println checkouts-to-rm)
+  (dorun
+   (map (comp fs/delete-dir (partial fs/file "checkouts")) checkouts-to-rm))
+  checkouts-to-rm)
 
 (defn rm
   "[pattern]: Remove all checkouts. If PATTERN is specified, only checkouts matching that pattern will be removed"
-  []
-  (println "Amazing rm action!"))
+  [{:keys [checkout] :as project} & [pattern]]
+  (let [current-checkouts (fs/list-dir "checkouts")
+        candidate-pattern (if pattern (re-pattern (str ".*" pattern ".*")) #".*")
+        candidate-matcher (partial re-matches candidate-pattern)
+        matching-checkouts (filter candidate-matcher current-checkouts)]
+    (if (= 0 (count matching-checkouts))
+      (report-no-matches candidate-pattern current-checkouts)
+      (rm-checkouts candidate-pattern matching-checkouts))
+    ))
