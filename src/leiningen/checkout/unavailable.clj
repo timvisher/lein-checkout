9d082b3cc84a048f25d4720230b72afa23bcb9da
Tim Visher <tvisher@relaynetwork.com>
2013-01-16 09:52:24 -0500

    Adding an `unavailable` task. EOM

---
+++ src/leiningen/checkout/unavailable.clj
@@ -0,0 +1,6 @@
+(ns leiningen.checkout.unavailable)
+
+(defn unavailable
+  "List declared dependencies for which no checkout can be made."
+  [project]
+  (println "Amazing unavailable listing action!"))
